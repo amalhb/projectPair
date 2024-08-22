a6fcd04c02905b7ffd600a263470e3b7a11c45db
amalhb <hbamal251@gmail.com>
2024-08-22 03:52:21 -0700

    1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # projectPair
-777777
---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('#add').on('click', function () {
+        $('.form-container').show();
+    });
+
+    $(document).on('click', '.cancel-btn', function () {
+        $('.form-container').hide();
+    });
+})
